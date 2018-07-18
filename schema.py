6604e04fc14d2e436e112f306ef819ab57a6c114
yargo13 <yargo_13@hotmail.com>
2018-07-17 23:27:30 -0300

    Create schema.py

---
+++ schema.py
@@ -0,0 +1,17 @@
+import graphene
+from graphene_sqlalchemy import SQLAlchemyConnectionField, SQLAlchemyObjectType
+from models import Student as StudentModel
+from database import db_session
+
+class Student(SQLAlchemyObjectType):
+    class Meta:
+        model = StudentModel
+
+class Query(graphene.ObjectType):
+    students = graphene.List(Student)
+
+    def resolve_students(self, info):
+        query = Todo.get_query(info)
+        return query.all()
+
+schema = graphene.Schema(query=Query)
